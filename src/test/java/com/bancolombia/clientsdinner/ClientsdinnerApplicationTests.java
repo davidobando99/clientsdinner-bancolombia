71558e72bdf6e266e98eca409ccf71bb5e6318a0
David Obando <david.obando2@nordstrom.com>
2021-10-23 09:40:24 -0500

    first commit

---
+++ src/test/java/com/bancolombia/clientsdinner/ClientsdinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.clientsdinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsdinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
